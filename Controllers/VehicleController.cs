40a6590eb8af54e3f66e8a1b6b365d75723a3a87
Thiago Miranda <marine.rlivielor@gmail.com>
2017-10-30 22:50:26 -0200

    Criação da lógica para o controller Vehicle

--- Controllers/VehicleController.cs
+++ Controllers/VehicleController.cs
@@ -1,7 +1,9 @@
+using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Vega_ASP.Net_Core.Controllers.Resources;
 using Vega_ASP.Net_Core.Models;
+using Vega_ASP.Net_Core.Persistence;
 
 namespace Vega_ASP.Net_Core.Controllers
 {
@@ -9,15 +11,22 @@ namespace Vega_ASP.Net_Core.Controllers
     public class VehicleController : Controller
     {
         private readonly IMapper mapper;
-        public VehicleController(IMapper mapper)
+        private readonly VegaDbContext context;
+        public VehicleController(IMapper mapper, VegaDbContext context)
         {
+            this.context = context;
             this.mapper = mapper;
         }
         [HttpPost]
-        public IActionResult Post([FromBody] VehicleResource vehicleResource)
+        public async Task<IActionResult> Post([FromBody] VehicleResource vehicleResource)
         {
             var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
-            return Ok(vehicle);
+            context.Vehicles.Add(vehicle);
+
+            await context.SaveChangesAsync();
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+            return Ok(result);
         }
     }
 }
